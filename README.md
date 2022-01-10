bb70063662d50c2f7dfa3fc440431c39351b4032
smebberson <scott@scottmebberson.com>
2022-01-10 17:01:20 +1030

    Tidied up files (and removed Vagrant).

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # art
+
 My digital art.
--- public/art/series/geometry/isosceles-grid/001/js/001.js
+++ public/art/series/geometry/isosceles-grid/001/js/001.js
@@ -1,172 +1,183 @@
+(function (w, d, el, attrs) {
+  // on load
+  d.addEventListener("DOMContentLoaded", function (event) {
+    window.setTimeout(function () {
+      // Create the canvas to draw on, and alias it globally
+      var c = (w.c = Snap("#" + el));
+      var rect = d.getElementById(el).getBoundingClientRect();
+      var colorsCount = 6;
 
-(function (w,d,el,attrs) {
+      // number of triangles on the x-axis
+      var xCount = Math.ceil(rect.width / attrs.triangle.width) + 1;
+      var yCount = Math.ceil(rect.height / attrs.triangle.height) + 1;
 
-    // on load
-    d.addEventListener("DOMContentLoaded", function (event) {
+      // determine the colours
+      // var color = chroma.scale('RdPu').out('hex');
+      // var color = chroma.scale([random(),random()]).mode('lch').out('hex');
 
-        window.setTimeout(function () {
+      // var color = chroma.scale(chroma.interpolate.bezier([randomColor(),randomColor()])).domain([1,4]).mode('lch').correctLightness(true).out('hex');
 
-            // Create the canvas to draw on, and alias it globally
-            var c = w.c = Snap("#canvas");
+      // tinycolor
+      var color = (function () {
+        var _colors = tinycolor(randomColor().hex()).monochromatic(
+          colorsCount + 4
+        );
 
-            var colorsCount = 6;
+        _colors
+          .sort(function (a, b) {
+            return (
+              deconstructColor({ hex: a.toHexString() }).luma -
+              deconstructColor({ hex: b.toHexString() }).luma
+            );
+          })
+          .reverse()
+          .pop();
 
-            // number of triangles on the x-axis
-            var xCount = Math.ceil(c.node.offsetWidth / attrs.triangle.width) + 1;
-            var yCount = Math.ceil(c.node.offsetHeight / attrs.triangle.height) + 1;
+        return function (index) {
+          index = Math.floor(index);
 
-            // determine the colours
-            // var color = chroma.scale('RdPu').out('hex');
-            // var color = chroma.scale([random(),random()]).mode('lch').out('hex');
+          var c = _colors[index];
 
-            // var color = chroma.scale(chroma.interpolate.bezier([randomColor(),randomColor()])).domain([1,4]).mode('lch').correctLightness(true).out('hex');
+          return c.toHexString();
+        };
+      })();
 
-            // tinycolor
-            var color = (function () {
+      window._color = color;
 
-                var _colors = tinycolor(randomColor().hex()).monochromatic(colorsCount+4);
+      // loop horizontally
+      for (var horz = 0; horz < xCount; horz++) {
+        for (var vert = 0; vert < yCount; vert++) {
+          // we do two triangles, one the right way up, and one upside down
 
-                _colors.sort(function (a, b) {
-                    return deconstructColor({'hex': a.toHexString()}).luma - deconstructColor({'hex': b.toHexString()}).luma;
-                }).reverse().pop();
+          // return a number, at-most 5
+          var triangleColor = color(Math.floor(Math.random() * colorsCount));
+          // var triangleColor = color(vert % colorsCount);
 
-                return function (index) {
+          // x,y
+          var triangle = c.paper
+            .polygon([
+              attrs.triangle.width / 2,
+              0,
+              attrs.triangle.width,
+              attrs.triangle.height,
+              0,
+              attrs.triangle.height,
+              attrs.triangle.width / 2,
+              0,
+            ])
+            .attr({
+              fill: triangleColor,
+              stroke: triangleColor,
+              "stroke-width": 0.6,
+            });
 
-                    index = Math.floor(index);
+          // position
+          triangle.transform(
+            "translate(" +
+              horz * attrs.triangle.width +
+              "," +
+              vert * attrs.triangle.height +
+              ")"
+          );
 
-                    var c = _colors[index];
+          // upside down triangle
+          triangleColor = color(Math.floor(Math.random() * colorsCount));
 
-                    return c.toHexString();
+          triangle = c.paper
+            .polygon([
+              0,
+              0,
+              attrs.triangle.width / 2,
+              attrs.triangle.height,
+              attrs.triangle.width,
+              0,
+              0,
+              0,
+            ])
+            .attr({
+              fill: triangleColor,
+              stroke: triangleColor,
+              "stroke-width": 0.6,
+            });
 
-                }
-
-            })();
-
-            window._color = color;
-
-            // loop horizontally
-            for (var horz = 0; horz < xCount; horz++) {
-
-                for (var vert = 0; vert < yCount; vert++) {
-
-                    // we do two triangles, one the right way up, and one upside down
-
-                    // return a number, at-most 5
-                    var triangleColor = color(Math.floor(Math.random()*colorsCount));
-                    // var triangleColor = color(vert % colorsCount);
-
-                    // x,y
-                    var triangle = c.paper.polygon([
-                            attrs.triangle.width/2,0,
-                            attrs.triangle.width,attrs.triangle.height,
-                            0,attrs.triangle.height,
-                            attrs.triangle.width/2,0
-                    ]).attr({
-                        'fill': triangleColor,
-                        'stroke': triangleColor,
-                        'stroke-width': 0.6
-                    });
-
-                    // position
-                    triangle.transform('translate(' + horz*attrs.triangle.width + ',' + vert*attrs.triangle.height + ')');
-
-                    // upside down triangle
-                    triangleColor = color(Math.floor(Math.random()*colorsCount));
-
-                    triangle = c.paper.polygon([
-                        0,0,
-                        attrs.triangle.width/2,attrs.triangle.height,
-                        attrs.triangle.width,0,
-                        0,0
-                    ]).attr({
-                        'fill': triangleColor,
-                        'stroke': triangleColor,
-                        'stroke-width': 0.6
-                    });
-
-                    // position
-                    triangle.transform('translate(' + ((horz*attrs.triangle.width) - attrs.triangle.width/2) + ',' + vert*attrs.triangle.height + ')');
-
-                }
-
-            }
-
-        }, 200);
-
-    });
-
-    window.randomColor = function (space) {
-
-        space = space || 'lch';
-
-        var digits = '0123456789abcdef',
-            code = "#";
-
-        for (var i = 0; i < 6; i++) {
-            code += digits.charAt(Math.floor(Math.random() * 16))
+          // position
+          triangle.transform(
+            "translate(" +
+              (horz * attrs.triangle.width - attrs.triangle.width / 2) +
+              "," +
+              vert * attrs.triangle.height +
+              ")"
+          );
         }
+      }
+    }, 200);
+  });
 
-        return chroma(code, space);
+  window.randomColor = function (space) {
+    space = space || "lch";
 
-    };
+    var digits = "0123456789abcdef",
+      code = "#";
 
-    window.deconstructColor = function (colorObj) {
-
-        //adapted from http://www.runtime-era.com/2011/11/grouping-html-hex-colors-by-hue-in.html
-        var hex = colorObj.hex.substring(1);
-        /* Get the RGB values to calculate the Hue. */
-        var r = parseInt(hex.substring(0, 2), 16) / 255;
-        var g = parseInt(hex.substring(2, 4), 16) / 255;
-        var b = parseInt(hex.substring(4, 6), 16) / 255;
-
-        /* Getting the Max and Min values for Chroma. */
-        var max = Math.max.apply(Math, [r, g, b]);
-        var min = Math.min.apply(Math, [r, g, b]);
-
-
-        /* Variables for HSV value of hex color. */
-        var chr = max - min;
-        var hue = 0;
-        var val = max;
-        var sat = 0;
-
-
-        if (val > 0) {
-            /* Calculate Saturation only if Value isn't 0. */
-            sat = chr / val;
-            if (sat > 0) {
-                if (r == max) {
-                    hue = 60 * (((g - min) - (b - min)) / chr);
-                    if (hue < 0) {
-                        hue += 360;
-                    }
-                } else if (g == max) {
-                    hue = 120 + 60 * (((b - min) - (r - min)) / chr);
-                } else if (b == max) {
-                    hue = 240 + 60 * (((r - min) - (g - min)) / chr);
-                }
-            }
-        }
-
-        colorObj.chroma = chr;
-        colorObj.hue = hue;
-        colorObj.sat = sat;
-        colorObj.val = val;
-        colorObj.luma = .3 * r + .59 * g + .11 * b
-        colorObj.red = r;
-        colorObj.green = g;
-        colorObj.blue = b;
-
-        return colorObj;
-
-    };
-
-})(window, document, "#canvas", {
-    triangle: {
-        width: 100,
-        height: 100
-    },
-    animation: {
-        rate: 20
+    for (var i = 0; i < 6; i++) {
+      code += digits.charAt(Math.floor(Math.random() * 16));
     }
+
+    return chroma(code, space);
+  };
+
+  window.deconstructColor = function (colorObj) {
+    //adapted from http://www.runtime-era.com/2011/11/grouping-html-hex-colors-by-hue-in.html
+    var hex = colorObj.hex.substring(1);
+    /* Get the RGB values to calculate the Hue. */
+    var r = parseInt(hex.substring(0, 2), 16) / 255;
+    var g = parseInt(hex.substring(2, 4), 16) / 255;
+    var b = parseInt(hex.substring(4, 6), 16) / 255;
+
+    /* Getting the Max and Min values for Chroma. */
+    var max = Math.max.apply(Math, [r, g, b]);
+    var min = Math.min.apply(Math, [r, g, b]);
+
+    /* Variables for HSV value of hex color. */
+    var chr = max - min;
+    var hue = 0;
+    var val = max;
+    var sat = 0;
+
+    if (val > 0) {
+      /* Calculate Saturation only if Value isn't 0. */
+      sat = chr / val;
+      if (sat > 0) {
+        if (r == max) {
+          hue = 60 * ((g - min - (b - min)) / chr);
+          if (hue < 0) {
+            hue += 360;
+          }
+        } else if (g == max) {
+          hue = 120 + 60 * ((b - min - (r - min)) / chr);
+        } else if (b == max) {
+          hue = 240 + 60 * ((r - min - (g - min)) / chr);
+        }
+      }
+    }
+
+    colorObj.chroma = chr;
+    colorObj.hue = hue;
+    colorObj.sat = sat;
+    colorObj.val = val;
+    colorObj.luma = 0.3 * r + 0.59 * g + 0.11 * b;
+    colorObj.red = r;
+    colorObj.green = g;
+    colorObj.blue = b;
+
+    return colorObj;
+  };
+})(window, document, "canvas", {
+  triangle: {
+    width: 100,
+    height: 100,
+  },
+  animation: {
+    rate: 20,
+  },
 });
