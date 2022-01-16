34752a046ffffa98f5bd0dae7b1f15aa3f1f278c
smebberson <scott@scottmebberson.com>
2022-01-17 08:05:50 +1030

    The artworks blueprint is now encoded to a string.

--- public/art/series/geometry/isosceles-grid/001/js/001.js
+++ public/art/series/geometry/isosceles-grid/001/js/001.js
@@ -1,4 +1,49 @@
+// #e96e81,#d06273,#b65665,#9d4a57,#833e49,#6a323b
+// d2u3d3u1d3u5d4u4d5u5d4u1d3u1d5u1d4u3d3u4d4u1d1u5d3u0d4u1d3u1d0u4d1u3d0u2d5u5d4u0d4u1d4u1d1u0
+// d3u3d0u4d1u3d0u0d4u5d3u1d3u2d2u1d5u4d2u3d3u0d3u0d1u5d1u3d3u1d4u2d1u1d0u3d1u3d3u4d4u4d4u1d5u1
+// d3u0d2u0d0u5d3u4d3u1d1u1d4u5d4u5d4u4d4u5d3u4d3u2d3u2d3u3d2u2d1u0d4u0d4u3d3u2d3u4d0u2d4u0d1u0
+// d5u1d4u5d1u1d0u1d1u3d0u5d0u5d2u3d0u0d4u1d4u3d5u2d3u4d2u4d2u3d5u1d1u5d3u5d1u4d1u3d1u5d3u5d3u3
+// d2u0d2u3d1u1d5u2d4u2d0u1d3u2d2u1d2u5d5u2d1u3d3u3d5u4d0u2d0u2d5u4d5u0d3u3d0u3d5u1d2u3d2u2d3u3
+// d0u0d5u0d5u5d2u2d0u4d5u2d5u4d0u3d2u3d3u3d2u5d5u4d1u2d5u5d1u4d0u5d0u2d0u3d1u3d5u3d3u4d3u1d4u3
+// d0u3d4u2d3u4d0u5d1u3d0u3d3u1d4u1d5u3d3u2d0u3d1u2d2u5d1u0d4u3d0u5d4u5d1u1d5u0d1u2d3u1d4u5d2u3
+// d3u4d1u2d0u2d3u0d5u4d0u5d3u2d4u4d2u0d5u1d2u0d3u1d2u2d4u1d3u0d3u1d5u4d4u4d3u2d5u3d2u0d2u2d1u1
+// d3u5d2u3d5u4d2u5d5u3d0u2d2u5d3u1d1u2d3u2d3u5d3u5d0u5d5u5d0u5d2u1d4u4d0u0d3u0d0u0d4u2d4u4d0u0
+// d1u3d3u1d1u4d1u5d2u4d4u3d4u1d5u3d0u0d3u2d3u1d4u5d5u4d2u3d1u2d4u1d0u4d2u2d2u5d4u3d0u5d2u4d4u3
+// d1u2d0u1d5u4d1u0d5u5d2u1d3u3d5u4d0u4d2u2d0u3d4u0d3u0d5u2d3u0d0u4d4u5d3u0d4u1d1u5d2u1d1u0d4u5
+// d1u0d4u3d1u1d1u2d5u3d4u3d2u5d0u5d2u1d5u2d3u3d5u2d2u4d1u1d1u3d4u0d4u2d3u0d2u3d5u2d0u4d4u4d5u1
+// d1u1d2u3d3u4d1u1d2u0d0u2d1u4d2u1d4u2d3u3d3u5d0u0d3u1d0u1d5u1d1u4d2u5d5u2d5u0d2u5d1u5d4u1d0u3
+// d1u1d5u0d2u0d3u2d2u3d3u3d4u4d1u3d5u5d2u1d5u2d1u5d4u1d4u0d0u2d0u1d4u4d4u3d1u0d0u2d3u0d1u0d1u2
+// d3u4d4u4d4u3d5u4d4u4d5u3d5u5d2u5d0u3d2u2d2u3d3u4d5u0d5u0d5u0d2u0d5u5d5u4d4u1d4u1d1u5d1u4d4u3
+// d5u5d0u5d1u5d1u2d3u0d4u2d2u0d2u1d5u2d0u4d3u1d4u1d4u4d4u1d1u0d5u1d4u3d3u3d3u3d2u4d4u5d4u4d5u3
+// d3u3d1u0d5u4d1u5d3u5d4u5d5u3d1u3d1u1d0u2d1u0d1u5d1u0d5u4d0u4d1u5d3u1d0u5d0u2d1u0d4u3d1u0d5u5
+// d2u0d4u3d3u5d5u1d5u0d3u2d3u3d0u0d2u1d2u4d4u0d3u4d4u4d5u2d4u5d2u1d0u4d3u0d5u5d5u3d3u3d4u1d4u2
+// d5u1d5u5d4u1d3u3d1u3d4u1d2u2d4u5d1u5d0u1d0u3d2u4d5u5d2u2d0u1d0u3d0u1d2u4d2u4d2u2d0u2d5u0d0u4
+// d2u2d3u5d5u2d2u2d0u4d4u2d1u4d4u1d0u4d3u4d1u2d1u1d5u3d5u3d0u5d3u0d5u3d0u0d5u4d0u3d2u1d0u4d5u2
+// d4u1d5u3d3u2d3u0d5u3d5u5d4u0d2u4d5u0d5u4d4u5d0u0d5u5d4u4d3u5d0u3d2u2d3u4d1u5d2u4d2u2d4u5d4u4
+// d5u5d2u2d4u4d2u4d0u0d4u5d1u4d0u1d5u0d5u1d4u5d0u0d4u4d2u2d2u0d4u0d2u5d0u4d3u4d0u1d3u1d0u0d2u5
+// d1u0d5u5d2u4d2u5d1u3d4u3d1u4d4u2d5u3d1u5d3u3d4u3d0u3d3u5d5u3d4u4d0u0d0u1d3u5d0u3d2u3d0u4d2u3
+// d1u1d0u0d0u4d0u1d4u4d1u1d4u3d3u0d5u2d2u3d5u4d0u3d4u4d2u3d4u3d5u5d5u3d3u2d1u4d2u3d0u0d4u3d3u5
+// d0u4d5u5d0u4d5u2d5u4d5u3d3u5d5u1d1u1d4u1d5u3d3u3d4u5d5u4d0u3d0u2d5u0d4u5d3u4d4u0d3u1d5u0d3u3
+// d2u5d5u2d2u3d0u0d4u4d1u0d5u1d2u3d1u3d0u3d3u1d4u1d5u0d5u4d3u1d1u3d1u0d3u1d0u0d3u3d3u3d0u3d5u1
+// d1u5d3u2d2u1d1u3d3u3d5u2d4u3d4u2d5u2d4u2d5u0d3u1d4u2d5u2d5u2d2u5d4u3d0u2d2u3d4u5d0u4d4u1d0u5
+// d0u4d5u5d4u0d0u2d5u3d5u1d2u4d4u2d5u1d5u0d0u0d4u2d1u2d1u1d3u4d0u1d5u0d4u3d0u2d0u0d4u5d1u1d2u1
+// d4u0d1u1d3u2d1u1d4u3d2u1d1u3d3u1d1u0d3u1d1u0d5u4d1u1d3u5d3u4d2u5d5u0d0u2d0u3d4u5d2u3d0u1d4u0
+// d1u4d3u3d1u1d2u2d4u1d0u4d1u5d4u2d5u0d3u3d5u5d3u4d3u2d2u0d4u4d2u4d5u0d3u2d5u4d4u2d3u4d2u3d3u4
+// d1u1d3u2d3u3d0u1d5u2d5u0d2u5d0u4d5u3d1u0d4u5d2u3d1u1d2u2d2u0d4u1d2u2d4u5d0u5d1u2d0u5d5u5d0u5
+// d5u2d2u1d0u1d4u0d2u5d5u2d0u2d5u3d4u3d5u3d3u3d0u5d2u4d0u2d0u4d3u0d2u1d3u1d4u3d2u2d4u1d5u1d0u2
+// d3u3d5u1d0u2d0u0d5u1d4u1d3u5d5u4d4u3d2u1d2u3d0u2d2u5d1u0d0u2d0u1d0u2d4u2d1u4d4u4d5u5d2u1d4u4
+// d0u3d2u4d4u0d4u5d1u2d0u1d4u3d1u3d1u5d0u0d0u4d1u4d4u2d1u2d1u5d5u0d5u1d5u5d3u2d0u4d3u0d1u4d5u2
+// d1u2d5u5d5u2d1u5d5u3d5u3d3u2d3u3d4u4d1u0d4u2d1u1d1u3d2u3d3u1d0u2d2u2d2u2d4u1d2u1d0u2d3u5d3u4
+// d3u3d0u2d5u4d1u1d5u4d3u1d3u1d5u5d3u4d0u3d0u2d0u0d5u2d1u4d4u2d5u3d5u1d2u4d2u1d3u4d1u0d0u3d5u4
+// d0u5d5u4d2u4d5u2d1u5d0u0d1u1d2u1d0u0d0u4d0u4d0u2d3u3d1u4d1u0d0u3d2u4d4u5d3u3d1u1d3u1d0u0d0u3
+// d4u3d3u4d0u5d1u3d1u5d4u0d4u5d3u1d0u1d2u3d5u1d4u4d2u1d0u0d5u3d3u2d4u2d0u1d4u3d1u4d2u2d5u1d5u1
+// d4u5d4u3d3u0d2u2d2u4d5u4d1u2d4u0d5u0d3u1d2u2d1u4d2u2d4u2d3u3d2u5d0u2d0u1d4u1d4u0d5u1d4u5d1u2
+// d1u4d5u0d3u4d1u0d3u1d5u1d0u4d2u0d2u4d0u0d2u1d2u5d1u0d0u4d2u3d1u2d0u3d0u1d4u5d5u2d1u4d2u0d2u5
+
 ((w, d, el, attrs) => {
+    let c = null;
+    let generated = '';
+
     const randomColor = () => {
         const digits = '0123456789abcdef';
         let code = '#';
@@ -57,107 +102,140 @@
         return colorObj;
     };
 
-    d.addEventListener('DOMContentLoaded', () => {
-        setTimeout(() => {
-            // Create the canvas to draw on, and alias it globally
-            const c = (w.c = Snap('#' + el));
+    d.addEventListener('keyup', (event) => {
+        if (event.key === 'D') {
             const rect = d.getElementById(el).getBoundingClientRect();
-            const colorsCount = 6;
+            console.log({
+                el,
+                height: rect.height,
+                name: w.name,
+                width: rect.width,
+            });
+        }
 
-            // number of triangles on the x-axis
-            const xCount = Math.ceil(rect.width / attrs.triangle.width) + 1;
-            const yCount = Math.ceil(rect.height / attrs.triangle.height) + 1;
+        if (event.key === 'P') {
+            generated = prompt('Paste in your artworks code.');
+            clear();
+            paint();
+        }
 
-            // determine the colours
-            // var color = chroma.scale('RdPu').out('hex');
-            // var color = chroma.scale([random(),random()]).mode('lch').out('hex');
+        if (event.key === 'C') {
+            if (navigator.clipboard) {
+                navigator.clipboard.writeText(generated);
+            }
+        }
+    });
 
-            // var color = chroma.scale(chroma.interpolate.bezier([randomColor(),randomColor()])).domain([1,4]).mode('lch').correctLightness(true).out('hex');
+    const clear = () => {
+        c.paper.clear();
+    };
 
-            // tinycolor
-            const color = (() => {
-                const _colors = tinycolor(randomColor().hex()).monochromatic(
-                    colorsCount + 4
+    const blueprint = () => {
+        const rect = d.getElementById(el).getBoundingClientRect();
+        const colors = tinycolor(randomColor().hex())
+            .monochromatic(10)
+            .sort(
+                (a, b) =>
+                    deconstructColor({ hex: a.toHexString() }).luma -
+                    deconstructColor({ hex: b.toHexString() }).luma
+            )
+            .reverse()
+            .slice(0, -4);
+
+        // number of triangles on the x-axis
+        const xCount = Math.ceil(rect.width / attrs.triangle.width) + 1;
+        const yCount = Math.ceil(rect.height / attrs.triangle.height) + 1;
+
+        generated += `${colors.map((hsl) => hsl.toHexString()).toString()}\n`;
+
+        // loop horizontally
+        for (let horz = 0; horz < xCount; horz++) {
+            if (horz > 0) {
+                generated += '\n';
+            }
+
+            for (let vert = 0; vert < yCount; vert++) {
+                // we do two triangles, one the right way up, and one upside down
+
+                // return a number, at-most 5
+                const triangleColorIndex = Math.floor(
+                    Math.random() * colors.length
                 );
 
-                _colors
-                    .sort(
-                        (a, b) =>
-                            deconstructColor({ hex: a.toHexString() }).luma -
-                            deconstructColor({ hex: b.toHexString() }).luma
-                    )
-                    .reverse()
-                    .pop();
+                // upside down triangle
+                const upsideDownTriangleColorIndex = Math.floor(
+                    Math.random() * colors.length
+                );
+                generated += `d${upsideDownTriangleColorIndex}`;
+                generated += `u${triangleColorIndex}`;
+            }
+        }
+    };
 
-                return (index) => _colors[Math.floor(index)].toHexString();
-            })();
+    const triangle = (c, type) =>
+        type === 'u'
+            ? c.paper.polygon([
+                  attrs.triangle.width / 2,
+                  0,
+                  attrs.triangle.width,
+                  attrs.triangle.height,
+                  0,
+                  attrs.triangle.height,
+                  attrs.triangle.width / 2,
+                  0,
+              ])
+            : c.paper.polygon([
+                  0,
+                  0,
+                  attrs.triangle.width / 2,
+                  attrs.triangle.height,
+                  attrs.triangle.width,
+                  0,
+                  0,
+                  0,
+              ]);
 
-            // loop horizontally
-            for (let horz = 0; horz < xCount; horz++) {
-                for (let vert = 0; vert < yCount; vert++) {
-                    // we do two triangles, one the right way up, and one upside down
+    const paint = () => {
+        const [colorsList, ...rows] = generated.split('\n');
+        const colors = colorsList.split(',');
+        const color = (index) => colors[Math.floor(index)];
 
-                    // return a number, at-most 5
-                    const triangleColor = color(
-                        Math.floor(Math.random() * colorsCount)
-                    );
+        rows.forEach((str, rowIndex) => {
+            const regex = /(?:u|d)[0-9]/gm;
+            let m;
 
-                    // Draw the triangle
-                    c.paper
-                        .polygon([
-                            attrs.triangle.width / 2,
-                            0,
-                            attrs.triangle.width,
-                            attrs.triangle.height,
-                            0,
-                            attrs.triangle.height,
-                            attrs.triangle.width / 2,
-                            0,
-                        ])
+            while ((m = regex.exec(str)) !== null) {
+                // This is necessary to avoid infinite loops with zero-width matches
+                if (m.index === regex.lastIndex) {
+                    regex.lastIndex++;
+                }
+
+                m.forEach((match, _, { index }) => {
+                    const colIndex = index === 0 ? 0 : index / 2;
+                    const [direction, colorIndex] = match.split('');
+                    const triangleColor = color(Number(colorIndex));
+                    const offset = -50;
+
+                    triangle(c, direction)
                         .attr({
                             fill: triangleColor,
                             stroke: triangleColor,
                             'stroke-width': 0.9,
                         })
                         .transform(
-                            'translate(' +
-                                horz * attrs.triangle.width +
-                                ',' +
-                                vert * attrs.triangle.height +
-                                ')'
+                            `translate(${
+                                (colIndex * attrs.triangle.width) / 2 + offset
+                            },${rowIndex * attrs.triangle.height})`
                         );
-
-                    // upside down triangle
-                    const upsideDownTriangleColor = color(
-                        Math.floor(Math.random() * colorsCount)
-                    );
-
-                    // Upside down triangle
-                    c.paper
-                        .polygon([
-                            0,
-                            0,
-                            attrs.triangle.width / 2,
-                            attrs.triangle.height,
-                            attrs.triangle.width,
-                            0,
-                            0,
-                            0,
-                        ])
-                        .attr({
-                            fill: upsideDownTriangleColor,
-                        })
-                        .transform(
-                            'translate(' +
-                                (horz * attrs.triangle.width -
-                                    attrs.triangle.width / 2) +
-                                ',' +
-                                vert * attrs.triangle.height +
-                                ')'
-                        );
-                }
+                });
             }
-        }, 20);
+        });
+    };
+
+    d.addEventListener('DOMContentLoaded', () => {
+        c = Snap('#' + el);
+        blueprint();
+        paint();
     });
 })(window, document, 'canvas', {
     triangle: {
