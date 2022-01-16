316978fe82323da737265d95d4d908bf0862f835
smebberson <scott@scottmebberson.com>
2022-01-17 08:42:14 +1030

    Reduced blueprint encoding string.

--- public/art/series/geometry/isosceles-grid/001/js/001.js
+++ public/art/series/geometry/isosceles-grid/001/js/001.js
@@ -1,44 +1,44 @@
-// #e96e81,#d06273,#b65665,#9d4a57,#833e49,#6a323b
-// d2u3d3u1d3u5d4u4d5u5d4u1d3u1d5u1d4u3d3u4d4u1d1u5d3u0d4u1d3u1d0u4d1u3d0u2d5u5d4u0d4u1d4u1d1u0
-// d3u3d0u4d1u3d0u0d4u5d3u1d3u2d2u1d5u4d2u3d3u0d3u0d1u5d1u3d3u1d4u2d1u1d0u3d1u3d3u4d4u4d4u1d5u1
-// d3u0d2u0d0u5d3u4d3u1d1u1d4u5d4u5d4u4d4u5d3u4d3u2d3u2d3u3d2u2d1u0d4u0d4u3d3u2d3u4d0u2d4u0d1u0
-// d5u1d4u5d1u1d0u1d1u3d0u5d0u5d2u3d0u0d4u1d4u3d5u2d3u4d2u4d2u3d5u1d1u5d3u5d1u4d1u3d1u5d3u5d3u3
-// d2u0d2u3d1u1d5u2d4u2d0u1d3u2d2u1d2u5d5u2d1u3d3u3d5u4d0u2d0u2d5u4d5u0d3u3d0u3d5u1d2u3d2u2d3u3
-// d0u0d5u0d5u5d2u2d0u4d5u2d5u4d0u3d2u3d3u3d2u5d5u4d1u2d5u5d1u4d0u5d0u2d0u3d1u3d5u3d3u4d3u1d4u3
-// d0u3d4u2d3u4d0u5d1u3d0u3d3u1d4u1d5u3d3u2d0u3d1u2d2u5d1u0d4u3d0u5d4u5d1u1d5u0d1u2d3u1d4u5d2u3
-// d3u4d1u2d0u2d3u0d5u4d0u5d3u2d4u4d2u0d5u1d2u0d3u1d2u2d4u1d3u0d3u1d5u4d4u4d3u2d5u3d2u0d2u2d1u1
-// d3u5d2u3d5u4d2u5d5u3d0u2d2u5d3u1d1u2d3u2d3u5d3u5d0u5d5u5d0u5d2u1d4u4d0u0d3u0d0u0d4u2d4u4d0u0
-// d1u3d3u1d1u4d1u5d2u4d4u3d4u1d5u3d0u0d3u2d3u1d4u5d5u4d2u3d1u2d4u1d0u4d2u2d2u5d4u3d0u5d2u4d4u3
-// d1u2d0u1d5u4d1u0d5u5d2u1d3u3d5u4d0u4d2u2d0u3d4u0d3u0d5u2d3u0d0u4d4u5d3u0d4u1d1u5d2u1d1u0d4u5
-// d1u0d4u3d1u1d1u2d5u3d4u3d2u5d0u5d2u1d5u2d3u3d5u2d2u4d1u1d1u3d4u0d4u2d3u0d2u3d5u2d0u4d4u4d5u1
-// d1u1d2u3d3u4d1u1d2u0d0u2d1u4d2u1d4u2d3u3d3u5d0u0d3u1d0u1d5u1d1u4d2u5d5u2d5u0d2u5d1u5d4u1d0u3
-// d1u1d5u0d2u0d3u2d2u3d3u3d4u4d1u3d5u5d2u1d5u2d1u5d4u1d4u0d0u2d0u1d4u4d4u3d1u0d0u2d3u0d1u0d1u2
-// d3u4d4u4d4u3d5u4d4u4d5u3d5u5d2u5d0u3d2u2d2u3d3u4d5u0d5u0d5u0d2u0d5u5d5u4d4u1d4u1d1u5d1u4d4u3
-// d5u5d0u5d1u5d1u2d3u0d4u2d2u0d2u1d5u2d0u4d3u1d4u1d4u4d4u1d1u0d5u1d4u3d3u3d3u3d2u4d4u5d4u4d5u3
-// d3u3d1u0d5u4d1u5d3u5d4u5d5u3d1u3d1u1d0u2d1u0d1u5d1u0d5u4d0u4d1u5d3u1d0u5d0u2d1u0d4u3d1u0d5u5
-// d2u0d4u3d3u5d5u1d5u0d3u2d3u3d0u0d2u1d2u4d4u0d3u4d4u4d5u2d4u5d2u1d0u4d3u0d5u5d5u3d3u3d4u1d4u2
-// d5u1d5u5d4u1d3u3d1u3d4u1d2u2d4u5d1u5d0u1d0u3d2u4d5u5d2u2d0u1d0u3d0u1d2u4d2u4d2u2d0u2d5u0d0u4
-// d2u2d3u5d5u2d2u2d0u4d4u2d1u4d4u1d0u4d3u4d1u2d1u1d5u3d5u3d0u5d3u0d5u3d0u0d5u4d0u3d2u1d0u4d5u2
-// d4u1d5u3d3u2d3u0d5u3d5u5d4u0d2u4d5u0d5u4d4u5d0u0d5u5d4u4d3u5d0u3d2u2d3u4d1u5d2u4d2u2d4u5d4u4
-// d5u5d2u2d4u4d2u4d0u0d4u5d1u4d0u1d5u0d5u1d4u5d0u0d4u4d2u2d2u0d4u0d2u5d0u4d3u4d0u1d3u1d0u0d2u5
-// d1u0d5u5d2u4d2u5d1u3d4u3d1u4d4u2d5u3d1u5d3u3d4u3d0u3d3u5d5u3d4u4d0u0d0u1d3u5d0u3d2u3d0u4d2u3
-// d1u1d0u0d0u4d0u1d4u4d1u1d4u3d3u0d5u2d2u3d5u4d0u3d4u4d2u3d4u3d5u5d5u3d3u2d1u4d2u3d0u0d4u3d3u5
-// d0u4d5u5d0u4d5u2d5u4d5u3d3u5d5u1d1u1d4u1d5u3d3u3d4u5d5u4d0u3d0u2d5u0d4u5d3u4d4u0d3u1d5u0d3u3
-// d2u5d5u2d2u3d0u0d4u4d1u0d5u1d2u3d1u3d0u3d3u1d4u1d5u0d5u4d3u1d1u3d1u0d3u1d0u0d3u3d3u3d0u3d5u1
-// d1u5d3u2d2u1d1u3d3u3d5u2d4u3d4u2d5u2d4u2d5u0d3u1d4u2d5u2d5u2d2u5d4u3d0u2d2u3d4u5d0u4d4u1d0u5
-// d0u4d5u5d4u0d0u2d5u3d5u1d2u4d4u2d5u1d5u0d0u0d4u2d1u2d1u1d3u4d0u1d5u0d4u3d0u2d0u0d4u5d1u1d2u1
-// d4u0d1u1d3u2d1u1d4u3d2u1d1u3d3u1d1u0d3u1d1u0d5u4d1u1d3u5d3u4d2u5d5u0d0u2d0u3d4u5d2u3d0u1d4u0
-// d1u4d3u3d1u1d2u2d4u1d0u4d1u5d4u2d5u0d3u3d5u5d3u4d3u2d2u0d4u4d2u4d5u0d3u2d5u4d4u2d3u4d2u3d3u4
-// d1u1d3u2d3u3d0u1d5u2d5u0d2u5d0u4d5u3d1u0d4u5d2u3d1u1d2u2d2u0d4u1d2u2d4u5d0u5d1u2d0u5d5u5d0u5
-// d5u2d2u1d0u1d4u0d2u5d5u2d0u2d5u3d4u3d5u3d3u3d0u5d2u4d0u2d0u4d3u0d2u1d3u1d4u3d2u2d4u1d5u1d0u2
-// d3u3d5u1d0u2d0u0d5u1d4u1d3u5d5u4d4u3d2u1d2u3d0u2d2u5d1u0d0u2d0u1d0u2d4u2d1u4d4u4d5u5d2u1d4u4
-// d0u3d2u4d4u0d4u5d1u2d0u1d4u3d1u3d1u5d0u0d0u4d1u4d4u2d1u2d1u5d5u0d5u1d5u5d3u2d0u4d3u0d1u4d5u2
-// d1u2d5u5d5u2d1u5d5u3d5u3d3u2d3u3d4u4d1u0d4u2d1u1d1u3d2u3d3u1d0u2d2u2d2u2d4u1d2u1d0u2d3u5d3u4
-// d3u3d0u2d5u4d1u1d5u4d3u1d3u1d5u5d3u4d0u3d0u2d0u0d5u2d1u4d4u2d5u3d5u1d2u4d2u1d3u4d1u0d0u3d5u4
-// d0u5d5u4d2u4d5u2d1u5d0u0d1u1d2u1d0u0d0u4d0u4d0u2d3u3d1u4d1u0d0u3d2u4d4u5d3u3d1u1d3u1d0u0d0u3
-// d4u3d3u4d0u5d1u3d1u5d4u0d4u5d3u1d0u1d2u3d5u1d4u4d2u1d0u0d5u3d3u2d4u2d0u1d4u3d1u4d2u2d5u1d5u1
-// d4u5d4u3d3u0d2u2d2u4d5u4d1u2d4u0d5u0d3u1d2u2d1u4d2u2d4u2d3u3d2u5d0u2d0u1d4u1d4u0d5u1d4u5d1u2
-// d1u4d5u0d3u4d1u0d3u1d5u1d0u4d2u0d2u4d0u0d2u1d2u5d1u0d0u4d2u3d1u2d0u3d0u1d4u5d5u2d1u4d2u0d2u5
+// #f09b68#d68b5d#bd7a52#a36a47#8a593c#704931
+// 2100524551310230555121223434141355154431304253
+// 2550433502130205350540152552405155554205150443
+// 2230302520145231230320233535034104353523330132
+// 5115234441220403513353104440044404113233313325
+// 3235455404244212230402252025014025431211124113
+// 1134401054322010342351043440323154033100122152
+// 3451311410150142553453535110005523512052345113
+// 5445004251342113110151145302444433232535300425
+// 3324222221053353041451310002440223001440310100
+// 1013032150355002340515345353130550504555225501
+// 4335222440045543314353302332200311405351135155
+// 2211352551023534030300013305243523312451231544
+// 3411515250325423231014152200544430052344413312
+// 3354510123331303350221214400310044333010114331
+// 5132550003113502120204043551410430310353344100
+// 3023205525305515504014550333544422004155450404
+// 3050204000135324042413152404350354220114532504
+// 1111234154503243504152245255205450541155552223
+// 5244013532501251552304152334542221541513032014
+// 4412111030422242021123544122134002505403542353
+// 0430423503402433153202335000021402241015525123
+// 5544534544423132230022453031154103210335431335
+// 5331342250413231152332332151051500252214445001
+// 2505554123350235020402045211023514402014013244
+// 2113123243134030201501121231000204205500105315
+// 3435451523201440415532400004532123551121440505
+// 2010311105353350404533504230331010132102142214
+// 5451434315514253550435112200351030053525423403
+// 2531143135521232152254122550302422225015004112
+// 0313550044034134321030215314135552522314155123
+// 3322132523102410024545055131055441312041242043
+// 3133403225453250414420430045404412325525404435
+// 0142140133343425003002324431253011300055111331
+// 1411401155201215331003310143345450420354042115
+// 0243354122413345401531350202350245253115211240
+// 3111340211251423010210400252521410352320434104
+// 4355120315001053325215052415341451354540322320
+// 2245321420252022053151214444421134352415344310
+// 2515415522452043400102005413022552551305204133
+// 3132311313530525212302112133405420514334224421
 
 ((w, d, el, attrs) => {
     let c = null;
@@ -146,7 +146,7 @@
         const xCount = Math.ceil(rect.width / attrs.triangle.width) + 1;
         const yCount = Math.ceil(rect.height / attrs.triangle.height) + 1;
 
-        generated += `${colors.map((hsl) => hsl.toHexString()).toString()}\n`;
+        generated += `${colors.map((hsl) => hsl.toHexString()).join('')}\n`;
 
         // loop horizontally
         for (let horz = 0; horz < xCount; horz++) {
@@ -166,8 +166,7 @@
                 const upsideDownTriangleColorIndex = Math.floor(
                     Math.random() * colors.length
                 );
-                generated += `d${upsideDownTriangleColorIndex}`;
-                generated += `u${triangleColorIndex}`;
+                generated += `${upsideDownTriangleColorIndex}${triangleColorIndex}`;
             }
         }
     };
@@ -197,38 +196,30 @@
 
     const paint = () => {
         const [colorsList, ...rows] = generated.split('\n');
-        const colors = colorsList.split(',');
+        const colors = colorsList
+            .substr(1)
+            .split('#')
+            .map((hex) => `#${hex}`);
         const color = (index) => colors[Math.floor(index)];
 
         rows.forEach((str, rowIndex) => {
-            const regex = /(?:u|d)[0-9]/gm;
-            let m;
+            str.split('').forEach((col, colIndex) => {
+                const direction = colIndex % 2 === 0 ? 'd' : 'u';
+                const triangleColor = color(Number(col));
+                const offset = -50;
 
-            while ((m = regex.exec(str)) !== null) {
-                // This is necessary to avoid infinite loops with zero-width matches
-                if (m.index === regex.lastIndex) {
-                    regex.lastIndex++;
-                }
-
-                m.forEach((match, _, { index }) => {
-                    const colIndex = index === 0 ? 0 : index / 2;
-                    const [direction, colorIndex] = match.split('');
-                    const triangleColor = color(Number(colorIndex));
-                    const offset = -50;
-
-                    triangle(c, direction)
-                        .attr({
-                            fill: triangleColor,
-                            stroke: triangleColor,
-                            'stroke-width': 0.9,
-                        })
-                        .transform(
-                            `translate(${
-                                (colIndex * attrs.triangle.width) / 2 + offset
-                            },${rowIndex * attrs.triangle.height})`
-                        );
-                });
-            }
+                triangle(c, direction)
+                    .attr({
+                        fill: triangleColor,
+                        stroke: triangleColor,
+                        'stroke-width': 0.9,
+                    })
+                    .transform(
+                        `translate(${
+                            (colIndex * attrs.triangle.width) / 2 + offset
+                        },${rowIndex * attrs.triangle.height})`
+                    );
+            });
         });
     };
 
